00b7f53751dba0f588ff53d4776e6970cb35edae
JINKE-BETAWA <watabekenji112@icloud.com>
2020-12-21 21:54:38 +0900

    サインアップ画面表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :kane_first_name, :kane_last_name, :birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASS"] 
     end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :kane_first_name, :kane_last_name, :birthday])
   end
 
 end
