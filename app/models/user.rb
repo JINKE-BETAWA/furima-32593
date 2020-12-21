62b968cecaaf07a9f88eab7438f771259a2a61db
JINKE-BETAWA <watabekenji112@icloud.com>
2020-12-22 01:31:48 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :kane_first_name, presence: true
+  validates :kane_last_name, presence: true
+  validates :birthday, presence: true
+
   has_many :items
   has_many :purchases
 
