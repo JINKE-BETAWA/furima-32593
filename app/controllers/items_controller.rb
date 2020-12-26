d83411fd80eaaae23d53ccbfd5bfc7e567e46ea6
JINKE-BETAWA <watabekenji112@icloud.com>
2020-12-26 22:34:19 +0900

    collection_select設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,35 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index ]
 
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
-    item = Item.new
+    @item = Item.new
+
+    @category = Category.all
+    @state = State.all
+    @cost = Cost.all
+    @area = Area.all
+    @day = Day.all
+
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :state_id, :cost_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
 
 end
