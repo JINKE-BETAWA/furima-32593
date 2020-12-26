0f96488c959ed422c9850154bd71ea2b6d92eb24
JINKE-BETAWA <watabekenji112@icloud.com>
2020-12-26 16:02:00 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
 
   with_options presence: true do
 
+
+
+
+
   end
 
 
@@ -9,4 +13,12 @@ class Item < ApplicationRecord
   has_one          :purchase
   belongs_to       :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :day
+
 end
