294b555c6ed801f08503c7d548d853723f6e2039
JINKE-BETAWA <watabekenji112@icloud.com>
2020-12-30 16:27:10 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def set_root
-    if current_user.id != @item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def item_params
