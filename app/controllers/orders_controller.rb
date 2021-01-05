1d0c2e0d255c2351e30c2beac0e4539ac54c20a7
JINKE-BETAWA <watabekenji112@icloud.com>
2021-01-06 03:47:26 +0900

    createアクション

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,20 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def create
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+ def order_params
+  params.require(:order).permit(:post_code, :area_id, :city, :street_number, :building_number, :phone_number, :user_id, :item_id)
+ end
+
 end
