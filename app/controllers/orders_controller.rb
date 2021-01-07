bdbd46f67f3f7c98ba886410819233d44eaee504
JINKE-BETAWA <watabekenji112@icloud.com>
2021-01-07 21:36:41 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @item_order = Order.new
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
