83c2c08d495efed8309cdf8cc520c89b31a96efb
JINKE-BETAWA <watabekenji112@icloud.com>
2021-01-08 12:15:28 +0900

    テストコード追加

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,12 +9,29 @@ RSpec.describe Order, type: :model do
     end
 
     context '商品購入ができる時' do
-      it '必要な情報を適切に入力すると、商品の購入ができること' do
+      it '必要な情報を適切に入力すると、商品の購入ができる事' do
+        expect(@item_order).to be_valid
+      end
+
+      it '建物名が抜けていても登録できる事' do
+        @item_order.building_number = ''
         expect(@item_order).to be_valid
       end
     end
 
     context '商品購入ができない時' do
+      it 'user_idが空では登録できない事' do
+        @item_order.user_id = ''
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空では登録できない事' do
+        @item_order.item_id = ''
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Item can't be blank")
+      end
+
       it 'クレジットカード情報は必須であり、正しいクレジットカードの情報で無いときは決済できない事' do
         @item_order.token = ''
         @item_order.valid?
@@ -39,6 +56,12 @@ RSpec.describe Order, type: :model do
         expect(@item_order.errors.full_messages).to include('Area Select')
       end
 
+      it 'area_idが1以外でないと登録できない事' do
+        @item_order.area_id = '1'
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Area Select')
+      end
+
       it '配送先の情報として、市区町村が必須である事' do
         @item_order.city = ''
         @item_order.valid?
@@ -59,6 +82,12 @@ RSpec.describe Order, type: :model do
 
       it '電話番号にはハイフンは不要で、11桁以内である事（09012345678となる）' do
         @item_order.phone_number = '0901234567899999'
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it '電話番号は英数混合では登録できない事' do
+        @item_order.phone_number = '090ABCD1234'
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include('Phone number is invalid')
       end
